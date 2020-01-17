db992aee99e975450ac491d4f7b0424af11096c9
yassine <ouali.yasine@gmail.com>
2020-01-18 00:15:58 +0100

    publishing the website

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "For personnal bloging only",
         "excerpt":"Personnal post A part of the blog to be used for subjects unrelated to ML / CS posts. Curently not used, for this page to appear un comment its corresponding lines in navigation.yaml ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/y-blog/personal/chocolate-chip-cookies/"
+        "url": "https://yassouali.github.io//y-blog/personal/chocolate-chip-cookies/"
       },{
         "title": "Main fucntionnalities of the site",
         "excerpt":"Just a post that contains all the styles and tricks to write blog posts using this template. Adding equation First MathJax support sould be set to true in the _config.yml file. markdown: kramdownmathjax: trueAnd then we can add equations: Here is an example MathJax inline rendering \\( 1/x^{2} \\), and...","categories": [],
         "tags": ["functionnalities","this is a tag"],
-        "url": "http://localhost:4000/y-blog/functionnlities/"
+        "url": "https://yassouali.github.io//y-blog/functionnlities/"
       }]
